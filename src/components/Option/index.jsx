416df16adbd7fe2f612682b9cac6a9e3e2c1090b
Eliška Štěpánová <stepanova.eliska01@gmail.com>
2025-05-13 23:56:20 +0200

    'DOne'

--- src/components/Option/index.jsx
+++ src/components/Option/index.jsx
@@ -1,11 +1,11 @@
 import { Icon } from '../Icon/index';
 import './style.css';
 
-export const Option = (props) => {
+export const Option = ({type, text, onSelected}) => {
   return (
-    <div className="option">
-      <Icon type={props.type} />
-      <div>{props.text}</div>
+    <div className="option" onClick={()=>onSelected(type)}>
+      <Icon type={type} />
+      <div>{text}</div>
     </div>
   );
 };
